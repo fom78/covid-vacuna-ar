d4d0500c7a55d98b4f78e5b26a38ad0dc87ddbc4
fom78 <fom78a@gmail.com>
2021-03-08 16:23:21 -0300

    Update json with Sinopharm

--- scripts/pruebas.js
+++ scripts/pruebas.js
@@ -7,16 +7,12 @@ const crearJson = require('./nuevo-json')
 
 const year = "2021"
 const month = "03"
-const day = "04"
+const day = "07"
 const fecha = `${year}${month}${day}`
 //const jsonFileName = `${year}${month}${day}.json`
 // dado un csv, haga el json correspondiente.
 hacertran(fecha)
     .then(()=>{
-const year = "2021"
-const month = "03"
-const day = "04"
-// const jsonFileName = `${year}${month}${day}.json`
 const jsonFileName = `${fecha}.json`
 const json = fs.readJSONSync(`./public/data/${jsonFileName}`)
 crearJson(json, jsonFileName)
