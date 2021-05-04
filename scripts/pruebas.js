f181129d86b82457c1d79f4119c4a1ee302ef6a4
fom78 <fom78a@gmail.com>
2021-05-05 01:19:13 +0200

    bug

--- scripts/pruebas.js
+++ scripts/pruebas.js
@@ -9,9 +9,8 @@ const year = "2021";
 const month = "05";
 
 //Colocar de que dia a que dia del mes arriba colocado se rehace todo.
-const dayInicial = 2;
-const dayFinal = 2;
-
+const dayInicial = 4;
+const dayFinal = 4;
 //Damos una fecha lee el csv y lo transforma a json "hacertran"
 //Luego si es exitoso lee el json y genera el nuevo para tipos de vacunas.
 for (let i = dayInicial; i <= dayFinal; i++) {
