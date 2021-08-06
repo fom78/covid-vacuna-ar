55306d25722c490571253bd0f6ae922ef976a305
Fernando <fom78a@gmail.com>
2021-08-06 09:13:14 -0300

    delete fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import { DefaultSeo } from 'next-seo'
 
 import SEO from 'config/seo'
-//import useFathom from 'hooks/useFathom'
 
 import 'styles/globals.css'
 
 function App ({ Component, pageProps }) {
-  //useFathom({ ID: 'MYEXKUNW', domains: ['covid-vacuna.app'] })
 
   return (
     <>
